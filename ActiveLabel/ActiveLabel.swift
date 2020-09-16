224b7c94f5d900538bacb2b8a3a8b3b86de093ef
Ryan Luas <ryan.luas@mav.farm>
2020-09-16 11:28:37 -0700

    update proj settings xc12

--- ActiveLabel/ActiveLabel.swift
+++ ActiveLabel/ActiveLabel.swift
@@ -226,6 +226,8 @@ typealias ElementTuple = (range: NSRange, element: ActiveElement, type: ActiveTy
             selectedElement = nil
         case .stationary:
             break
+        case .regionEntered, .regionMoved, .regionExited:
+            break
         @unknown default:
             break
         }
